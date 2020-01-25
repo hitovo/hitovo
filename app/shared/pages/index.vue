57364a09619e34697df8b88a943135c9806fce4b
eliskaklimtova <eli.klimtova@gmail.com>
2020-01-25 17:06:40 +0100

    Feature: fetch data from backend

--- app/shared/pages/index.vue
+++ app/shared/pages/index.vue
@@ -1,11 +1,15 @@
 <template>
 	<div id="app">
-		<!-- Create the app here :) -->
-		<select v-model="selectedRatio" @change="valueChange('water')">
-			<option v-for="ratio in ratios" :key="ratio.label" :value="ratio.label">{{ ratio.label }}</option>
-		</select>
-		<input v-model="coffeeAmount" type="number" @change="valueChange('water')">
-		<input v-model="waterAmount" type="number" @change="valueChange('coffee')">
+		<template v-if="ratios.length">
+			<select v-model="selectedRatio" @change="valueChange('water')">
+				<option v-for="ratio in ratios" :key="ratio.label" :value="ratio.label">{{ ratio.label }}</option>
+			</select>
+			<input v-model="coffeeAmount" type="number" @change="valueChange('water')" />
+			<input v-model="waterAmount" type="number" @change="valueChange('coffee')" />
+		</template>
+		<template v-else>
+			<h2>404 No coffee found</h2>
+		</template>
 	</div>
 </template>
 
@@ -13,28 +17,27 @@
 	export default {
 		data() {
 			return {
-				ratios: [
-					{
-						label: 'V60',
-						value: [3, 50]
-					},
-					{
-						label: 'Chemex',
-						value: [1, 17]
-					}
-				],
+				ratios: [],
 				waterAmount: '0',
 				coffeeAmount: '15',
 				selectedRatio: 'V60'
 			};
 		},
 		mounted() {
-			this.valueChange('water');
+			this.loadRatios().then(() => {
+				this.valueChange('water');
+			});
 		},
 		methods: {
+			loadRatios() {
+				return fetch('/api/v1/ratios')
+					.then((response) => response.json())
+					.then(({ data: ratios }) => {
+						this.ratios = ratios;
+					});
+			},
 			valueChange(toChange) {
 				let ratio = this.ratios.find((item) => item.label === this.selectedRatio);
-
 				if (toChange === 'water') {
 					this.waterAmount = String(Math.round(Number(this.coffeeAmount) / (ratio.value[0] / ratio.value[1])));
 				} else if (toChange === 'coffee') {
